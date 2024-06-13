34bac960443ce5384efdcb9aded0c641aa82cedb
Farman Ahmed <farmanahmed888@gmail.com>
2024-06-14 01:17:55 +0530

    dummy commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'video_capture_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Video Capture',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const VideoCaptureScreen(),
+    );
+  }
+}
