464e3ead86d75b90e2abfc80a0f22d0421cb143f
Farman Ahmed <farmanahmed888@gmail.com>
2024-06-27 11:38:27 +0530

    chore: Remove unused import statement in video_capture_screen.dart

--- lib/video_capture_screen.dart
+++ lib/video_capture_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:camera/camera.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
